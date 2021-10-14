388e3c9a6755d3f20823ecb138b4cdf336046dd1
aparnasj-sj <61081490+aparnasj-sj@users.noreply.github.com>
2021-10-14 19:22:28 +0530

    Add files via upload

--- py1.py
+++ py1.py
@@ -1,22 +1,34 @@
-'''
-Find number of job openings in Bangalore,IN and in Seattle,US?
-Print the Number of Job opening in Bangalore and Seattle as Integer value.
-'''
-
-
-
 import csv
 job={}
+count=0
+lang=''
+maxC=0
+countC=0
+countP=0
+countJ=0
 with open('amazon_jobs_dataset.csv') as fo:
     fdata=csv.DictReader(fo,skipinitialspace=True)
     flist=list(fdata)#list of orederedDict
     for row in flist:
-        val=row['location']
-        if val in job:
-            job[val]+=1
-        else:
-            job[val]=1
-#print(1856,66)
-print(job['IN, KA, Bangalore '], job['US, WA, Seattle '])
-
-#print(job)
+        c=row['location'].split(',')[0] # split return list of tems split at given characer 
+        if(c=='IN'):
+        
+            q=row['BASIC QUALIFICATIONS']
+            if('Bachelor' in q or 'BS' in q or 'BA' in q):
+                if('C++' in q):
+                    countC+=1
+                    if countC>maxC:
+                        maxC=countC
+                        lang='c++'
+                if('Java' in q):
+                    countJ+=1
+                    if countJ>maxC:
+                        maxC=countJ
+                        lang='Java'
+                if('Python' in q):
+                    countP+=1
+                    if countP>maxC:
+                        maxC=countP
+                        lang='Python'
+print(lang,maxC)
+            
