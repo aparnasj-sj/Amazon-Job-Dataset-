bfbaadd78d2befeca99ddfcbae98c67aca77fa66
aparnasj-sj <61081490+aparnasj-sj@users.noreply.github.com>
2021-10-14 19:20:37 +0530

    Add files via upload

---
+++ py1.py
@@ -0,0 +1,15 @@
+import csv
+job={}
+with open('amazon_jobs_dataset.csv') as fo:
+    fdata=csv.DictReader(fo,skipinitialspace=True)
+    flist=list(fdata)#list of orederedDict
+    for row in flist:
+        val=row['location']
+        if val in job:
+            job[val]+=1
+        else:
+            job[val]=1
+#print(1856,66)
+print(job['IN, KA, Bangalore '], job['US, WA, Seattle '])
+
+#print(job)
